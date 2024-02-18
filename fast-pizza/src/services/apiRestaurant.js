f819d2eb434cb9a83833b00a2850c498902da956
Vaibhav wagh <vaibhavwagh393@gmail.com>
2024-02-18 10:44:12 +0530

    after tailwind css

--- fast-pizza/src/services/apiRestaurant.js
+++ fast-pizza/src/services/apiRestaurant.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_BASE_URL;
+const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
